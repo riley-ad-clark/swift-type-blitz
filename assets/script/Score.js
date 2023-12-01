8450ff4be6e128a0319982bd6e71689887289401
Riley Clark <r1l3ycl4rk@gmail.com>
2023-12-01 09:05:15 -0600

    added Score class

--- assets/script/Score.js
+++ assets/script/Score.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class Score {
+    #date;
+    #points;
+    #percentage;
+
+    constructor(date, points, percentage) {
+        this.#date = date;
+        this.#points = points;
+        this.#percentage = percentage;
+    }
+
+    get date(){ return this.#date };
+    get points() { return this.#points };
+    get percentage() { return this.#percentage };
+
+    get allInfo() {
+        return [this.#date, this.#points, this.#percentage];
+    }
+}
+
+export { Score }
